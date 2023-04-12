1b59699f9aa955ff07c4574fa3fb39155612437e
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-04-12 17:42:55 +0000

    check for errors

--- compile.js
+++ compile.js
@@ -1,2 +1,2 @@
 const { readFileSync } = require("fs")
-process.stdout.write('new Function("window", atob("' + readFileSync(0).toString('base64') + '"))(window)')
+process.stdout.write('try{new Function("window", atob("' + readFileSync(0).toString('base64') + '"))(window)}catch(e){alert("Antirandom setup error:\n" + e.stack)}')
