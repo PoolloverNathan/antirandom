1b67734007b64f6123c29bc283b11e86b7858e8b
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-04-12 18:52:06 +0000

    Add lite version

---
+++ lite.js
@@ -0,0 +1,41 @@
+if (document.overlay_antirandom_main) {
+  alert("already loaded")
+  return
+}
+
+const el = document.createElement("div")
+el.innerHtml = `
+<div style="height: 1cm;">
+  <label><input type="checkbox" id="arand-active"> Active</label>
+</div>
+<div style="height: 1cm;">
+  <input type="range" min="0" max="1" disabled id="arand-value" value="${Math.random()}">
+</div>
+`
+Object.assign(el.style, {
+  position: "fixed",
+  right: 0,
+  top: "1cm",
+  background: "purple",
+  borderBottom: "medium solid darkpurple",
+  borderLeft: "medium solid darkpurple",
+  borderBottomLeftRadius: "4px",
+  display: "flex",
+  alignItems: "center",
+  flex: "column",
+  height: "2cm",
+  width: "1in",
+  zIndex: 99999999
+})
+el.id = "overlay_antirandom_main"
+document.body.appendChild(el)
+
+const origRand = window.antirandomOrigRand || (window.antirandomOrigRand = Math.random)
+const shimRand = () => v
+
+const a = el.getElementById("arand-active")
+const v = el.getElementById("arand-value")
+a.onchange = () => {
+  v.disabled = !a.checked
+  Math.random = a.checked ? shimRand : origRand
+}
